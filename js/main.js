2ed888530257092460634eaeb2a088d6730cea1a
12andrian85 <118369529+12andrian85@users.noreply.github.com>
2024-02-05 05:36:57 +0200

    add layout for home page

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Import the Bootstrap bundle
+//
+// Includes Popper and all of Bootstrap's JS plugins.
+// import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+// Custom JS here
